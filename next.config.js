e76c7f2de30d7d9526b089e57affe1f70206823c
naluwan <street.baller06@gmail.com>
2023-10-16 16:19:03 +0800

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   images: {
     domains: ['utfs.io'],
   },
+  assetPrefix: !debug ? 'https://naluwan.github.io/Udemy-Clone/' : '',
 };
 
 module.exports = nextConfig;
