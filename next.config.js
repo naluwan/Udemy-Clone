35110f9d81329206bad92a69c488e0e5e2c88495
naluwan <street.baller06@gmail.com>
2023-10-16 19:19:24 +0800

    feat: add node version to package.json and add Profile for heroku

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   images: {
     domains: ['utfs.io'],
   },
-  assetPrefix: !debug ? 'https://naluwan.github.io/Udemy-Clone/' : '',
 };
 
 module.exports = nextConfig;
